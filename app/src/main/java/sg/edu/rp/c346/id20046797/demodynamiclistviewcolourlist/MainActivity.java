be65a944cbda4695cfb3967bbbffd35cc6b91df0
TheSchoolFriday <20046797@myrp.edu.sg>
2021-06-04 14:30:29 +0800

    added all the challenges

--- app/src/main/java/sg/edu/rp/c346/id20046797/demodynamiclistviewcolourlist/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20046797/demodynamiclistviewcolourlist/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +17,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     EditText etElement, etPosition;
-    Button btnAdd;
+    Button btnAdd, btnRmv, btnUpd;
     ListView lvColour;
 
     ArrayList<String> alColours = new ArrayList<>();
@@ -30,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         etPosition = findViewById(R.id.editTextPos);
 
         btnAdd = findViewById(R.id.buttonAddItem);
+        btnRmv = findViewById(R.id.buttonRemoveItem);
+        btnUpd = findViewById(R.id.buttonUpdateItem);
+
         lvColour = findViewById(R.id.listViewColour);
 
         alColours.add("Red");
@@ -42,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Boolean cBoolean = false;
                 String addItem = "";
-                int Number;
+                int iposition;
 
                 if(!(etElement.getText().toString().isEmpty())) {
                     addItem = etElement.getText().toString();
@@ -50,18 +54,81 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 if(etPosition.getText().toString().isEmpty()) {
-                    Number = alColours.size();
+                    iposition = alColours.size();
                     Toast.makeText(MainActivity.this, "Position number was not specified, assuming Last Position.", Toast.LENGTH_SHORT).show();
                 } else {
-                    Number = Integer.parseInt(etPosition.getText().toString());
+                    iposition = Integer.parseInt(etPosition.getText().toString());
                 }
 
                 if (cBoolean) {
-                    alColours.add(Number, addItem);
+                    alColours.add(iposition, addItem);
                     aaColour.notifyDataSetChanged();
                 } else {
                     Toast.makeText(MainActivity.this, "Colour not Specified, nothing was added.", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        btnRmv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int iposition = 0;
+                Boolean cBoolean = false;
+
+                if(!(etPosition.getText().toString().isEmpty())) {
+                    iposition = Integer.parseInt(etPosition.getText().toString());
+                    cBoolean = true;
+                }
+
+                if (iposition >= alColours.size()) { // Does not allow the user enter to big of a number (larger than the size of the Index)
+                    Toast.makeText(MainActivity.this, "Index is too large.", Toast.LENGTH_SHORT).show();
+                    cBoolean = false;
+                }
+
+                if (cBoolean) {
+                    Toast.makeText(MainActivity.this, "You have deleted the item: " + alColours.get(iposition), Toast.LENGTH_SHORT).show();
+                    alColours.remove(iposition);
+                    aaColour.notifyDataSetChanged();
+                }
+            }
+        });
+
+        btnUpd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Boolean cBoolean = false;
+                String addItem = "";
+                int iposition = 0;
+
+                if(!(etElement.getText().toString().isEmpty())) {
+                    addItem = etElement.getText().toString();
+                    cBoolean = true;
+                }
+
+                if(!(etPosition.getText().toString().isEmpty())) {
+                    iposition = Integer.parseInt(etPosition.getText().toString());
+                    cBoolean = true;
+                }
+
+                if (iposition >= alColours.size()) { // Does not allow the user enter to big of a number (larger than the size of the Index)
+                    Toast.makeText(MainActivity.this, "Index is too large.", Toast.LENGTH_SHORT).show();
+                    cBoolean = false;
+                }
+
+                if (cBoolean) {
+                    Toast.makeText(MainActivity.this, "Replacing Item: " + alColours.get(iposition) + " With: " + addItem, Toast.LENGTH_SHORT).show();
+                    alColours.set(iposition, addItem);
+                    aaColour.notifyDataSetChanged();
+                }
+            }
+        });
+
+
+        lvColour.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = alColours.get(position);
+                Toast.makeText(MainActivity.this, "You have selected the Colour " + selectedItem, Toast.LENGTH_SHORT).show();
             }
         });
     }
